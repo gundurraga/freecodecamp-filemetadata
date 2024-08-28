e864ad64abb630afb516ae136fc4880e95fa3dd1
gundurraga <67235552+gundurraga@users.noreply.github.com>
2024-08-28 17:11:53 -0400

    first commit

--- index.js
+++ index.js
@@ -1,20 +1,31 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
+require("dotenv").config();
 
 var app = express();
+var upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
+app.post("/api/fileanalyse", upload.single("upfile"), function (req, res) {
+  if (!req.file) {
+    return res.status(400).send("No file uploaded.");
+  }
 
-
+  res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size,
+  });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
